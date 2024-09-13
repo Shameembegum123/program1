156197a1a50a10807dea64f7b8593d2b18d10fac
DELL <DELL@192.168.1.4>
2024-09-13 18:06:21 +0530

    commit message

---
+++ corejavaprogram1/src/com/tns/encapsulation/Encapsulation.java
@@ -0,0 +1,25 @@
+package com.tns.encapsulation;
+// demo encapsulation
+public class Encapsulation {
+
+	int serialnumber;  //properties or data members
+	String name;
+	int age;
+	
+	
+	public void show()  {  //method or member function  
+		System.out.println("serial number   "+serialnumber+"    "  +"name   "+name+" "+"age   "+age);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ corejavaprogram1/src/com/tns/encapsulation/Exce.java
@@ -0,0 +1,19 @@
+package com.tns.encapsulation;
+
+public class Exce {
+
+	public static void main(String[] args) {
+		Encapsulation e=new Encapsulation();
+		e.serialnumber=1;
+		e.name="shameem";
+		e.age=19;
+		
+		
+		e.show(); //accessing the member function or method
+		
+	}
+	
+	
+	
+	
+}
